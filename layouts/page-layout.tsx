f12fb08f37aade7dbc00f047ec8f88fcbc2b17d6
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-02-23 14:42:01 +0700

    change classname to non require

--- layouts/page-layout.tsx
+++ layouts/page-layout.tsx
@@ -5,7 +5,7 @@ import Footer from '@/components/footer/Footer'
 
 interface PageLayoutProp {
   children: ReactNode
-  className: string
+  className?: string
   isHaveMenu?: boolean
   isHaveFooter?: boolean
   isFullHeight?: boolean
