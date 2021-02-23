60f459f13465a5df64ef1706da9bcb809e310dd6
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-02-23 14:33:08 +0700

    add emotion to last text

--- components/blog/BlogBody.tsx
+++ components/blog/BlogBody.tsx
@@ -4,10 +4,9 @@ type Props = {
 
 export default function BlogBody({ content }: Props): JSX.Element {
   return (
-    <div className="container">
-      <div className="max-w-2xl py-10 mx-auto">
-        <div className="markdown" dangerouslySetInnerHTML={{ __html: content }} />
-      </div>
+    <div className="max-w-2xl py-10 mx-auto">
+      <div className="markdown" dangerouslySetInnerHTML={{ __html: content }} />
+      <p className="text-center text-3xl">😊</p>
     </div>
   )
 }
