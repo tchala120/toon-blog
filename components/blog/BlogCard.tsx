1593e96972ec8723b4fbc4174b52842214ee7125
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-02-24 08:50:40 +0700

    add component for blog card

---
+++ components/blog/BlogCard.tsx
@@ -0,0 +1,20 @@
+import { BlogInfo } from '@/libs/blog'
+import React from 'react'
+import readingTime from 'reading-time'
+import { formatDate, limitTextAndEllipse } from 'utils/formatter'
+
+export default function BlogCard({ title, excerpt, timestamp, content }: BlogInfo): JSX.Element {
+  return (
+    <div className="blog-card">
+      <h1 className="text-gray-500 text-xs leading-normal mb-2">{title}</h1>
+      <p className="font-bold text-base">{limitTextAndEllipse(excerpt, 70)}</p>
+      <div className="flex flex-row  items-center text-gray-400 pt-2">
+        <small>{formatDate(timestamp, 'DD MMM YY')}</small>
+        <small className="mx-2">-</small>
+        <small className="flex items-center">
+          {readingTime(content).text}.<span className="ml-2 text-xl">☕</span>
+        </small>
+      </div>
+    </div>
+  )
+}
