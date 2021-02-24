f8f5c3fabc3cfea83002042443e506a997423355
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-02-24 08:53:12 +0700

    ellipse text dynamic

--- utils/formatter.ts
+++ utils/formatter.ts
@@ -5,5 +5,5 @@ export function formatDate(date: number | string | Date, format = 'DD/MM/YYYY HH
 }
 
 export function limitTextAndEllipse(text: string, length: number): string {
-  return text.substring(0, length).concat('...')
+  return text.length < length ? text : text.substring(0, length).concat('...')
 }
