995d5a8f66004349dc75f91f8f4e6f8955c095c4
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-02-24 08:51:12 +0700

    add prism plugin for markdown highlight

--- libs/markdownToHtml.ts
+++ libs/markdownToHtml.ts
@@ -1,7 +1,8 @@
 import remark from 'remark'
 import html from 'remark-html'
+import * as prism from 'remark-prism'
 
 export default async function markdownToHtml(markdown: string): Promise<string> {
-  const result = await remark().use(html).process(markdown)
+  const result = await remark().use(html).use(prism).process(markdown)
   return result.toString()
 }
