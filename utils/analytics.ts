c55c87d886e7d04e4163085036d95d9e80460fdf
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-13 04:06:48 +0700

    analytics file for handler ga

---
+++ utils/analytics.ts
@@ -0,0 +1,32 @@
+import ReactGA from 'react-ga'
+import { config } from 'dotenv'
+
+config()
+
+export const initGA = (): void => {
+  ReactGA.initialize(process.env.GA)
+}
+export const logPageView = (): void => {
+  ReactGA.set({ page: window.location.pathname })
+  ReactGA.pageview(window.location.pathname)
+}
+export const logEvent = (category = '', action = ''): void => {
+  if (category && action) {
+    ReactGA.event({ category, action })
+  }
+}
+export const logException = (description = '', fatal = false): void => {
+  if (description) {
+    ReactGA.exception({ description, fatal })
+  }
+}
+
+function setupGA(): void {
+  if (!window.GA_INITIALIZED) {
+    initGA()
+    window.GA_INITIALIZED = true
+  }
+  logPageView()
+}
+
+export default setupGA
