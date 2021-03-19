7c0ec4e79a26f1b67c36960726b4a891dfbd6649
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-20 03:56:26 +0700

    changed list to project card

--- constants/projects.ts
+++ constants/projects.ts
@@ -1,4 +1,4 @@
-import { Status } from '@/components/ui/List'
+import { Status } from '@/components/ui/ProjectCard'
 
 export interface IProject {
   id: number
@@ -14,7 +14,7 @@ export const projects: IProject[] = [
     id: 0,
     img: 'https://raw.githubusercontent.com/peterthehan/peterthehan/master/assets/discord.svg',
     name: 'Tconomy',
-    description: 'A discord bot about economy',
+    description: 'A discord bot about economy.',
     status: Status.NOT_FINISH,
     url: 'https://github.com/tchala120/discord_bot',
   },
@@ -22,7 +22,7 @@ export const projects: IProject[] = [
     id: 1,
     img: '/assets/tcoin_icon.svg',
     name: 'Tcoin',
-    description: 'A simple cryptocurrency',
+    description: 'A simple cryptocurrency API.',
     status: Status.ONLINE,
     url: 'https://github.com/tchala120/simple-blockchain',
   },
@@ -30,7 +30,7 @@ export const projects: IProject[] = [
     id: 2,
     img: '/assets/graphql-auth.svg',
     name: 'T-Core',
-    description: 'An authentication api with jwt',
+    description: 'An authentication API with graphql.',
     status: Status.ONLINE,
     url: 'https://github.com/tchala120/t-core',
   },
