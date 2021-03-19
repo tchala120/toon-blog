cfec74eaf326f0ea3d96f55502302af83a86077b
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-19 22:47:35 +0700

    new project about authentication with graphql

--- components/pages/Project.tsx
+++ components/pages/Project.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
+
 import Template from './Template'
 
-import { projects } from '../../constants/projects'
-import List from '../ui/List'
+import { Project, projects } from '@/constants/projects'
+
+import List from '@/components/ui/List'
 
 export default function Project(): JSX.Element {
   return (
-    <div className="other">
+    <div className="other my-10">
       <Template
         title="My Projects"
         icon={<span>💻 </span>}
         content="My whole project that i have done before and publish to everyone."
       />
       <div className="w-full mt-10 grid grid-cols-1 sm:grid-cols-2 gap-8">
-        {projects.map((p) => (
+        {projects.map((p: Project) => (
           <List key={p.id} {...p} />
         ))}
       </div>
--- constants/projects.ts
+++ constants/projects.ts
@@ -1,18 +1,37 @@
 import { Status } from '@/components/ui/List'
 
-export const projects = [
+export interface Project {
+  id: number
+  img: string
+  name: string
+  description: string
+  status: Status
+  url: string
+}
+
+export const projects: Project[] = [
   {
     id: 0,
     img: 'https://raw.githubusercontent.com/peterthehan/peterthehan/master/assets/discord.svg',
-    name: 'Tconomy Bot',
+    name: 'Tconomy',
+    description: 'A discord bot about economy',
     status: Status.NOT_FINISH,
     url: 'https://github.com/tchala120/discord_bot',
   },
   {
     id: 1,
     img: '/assets/tcoin_icon.svg',
-    name: 'A Simple Tcoin',
+    name: 'Tcoin',
+    description: 'A simple cryptocurrency',
     status: Status.ONLINE,
     url: 'https://github.com/tchala120/simple-blockchain',
+  },
+  {
+    id: 2,
+    img: '/assets/graphql-auth.svg',
+    name: 'T-Core',
+    description: 'An authentication api with jwt',
+    status: Status.ONLINE,
+    url: 'https://github.com/tchala120/t-core',
   },
 ]
